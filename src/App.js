68c7b4f9816a7b181473f32867b905d5e7155b52
Chotipong Wattanajindalert <jab.chotipong@gmail.com>
2021-09-05 18:48:48 +0700

    ignite first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+//Components and pages
+import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
